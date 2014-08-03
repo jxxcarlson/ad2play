f97ff724ef70a9bb41d4b366f1f18a585b4cc343
James Carlson <jxxcarlson@mac.com>
2014-08-03 00:48:55 -0400

    Added user message to install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ cp asciidoctor.css /usr/local/bin/
 cp ad2play /usr/local/bin/
 cp ad2html /usr/local/bin/
 cp ad2play-install.settings /usr/local/bin/ad2play.settings
+
+echo
+echo "ad2play and ad2html installedin /usr/local/bin"
+echo
+
